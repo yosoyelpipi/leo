7f14e38f6654ac4593cee1f8d87f82054d53e2ec
yosoyelpipi <lcondori@gmail.com>
2014-11-24 20:30:52 -0300

    Acelerómetro

--- js/mipropiojava.js
+++ js/mipropiojava.js
@@ -6,4 +6,17 @@ function onBodyLoad(){
 
 function onDeviceReady(){
 	$("#resultado").html("Phonegap está en funcionamiento.");
+
+	navigator.accelerometer.getCurrentAcceleration(onInfo, onError);
+}
+
+function onInfo(aceleracion){
+$("#resultado").append( 'Acceleration X: ' + aceleracion.x + '\n' +
+          				'Acceleration Y: ' + aceleracion.y + '\n' +
+          				'Acceleration Z: ' + aceleracion.z + '\n' +
+          				'Timestamp: '      + aceleracion.timestamp + '\n');
+}
+
+function onError(){
+	alert("No hay acelerómeto.");
 }
