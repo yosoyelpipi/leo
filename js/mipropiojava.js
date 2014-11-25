9edf909217af67ed316b4012185e12b2c0401f2a
yosoyelpipi <lcondori@gmail.com>
2014-11-24 22:07:59 -0300

    Audio
    Nace la botonera

--- js/mipropiojava.js
+++ js/mipropiojava.js
@@ -22,3 +22,69 @@ $("#resultado").html( 'Acceleration X: ' + aceleracion.x + '<br>' +
 function onError(){
 	alert("No hay acelerómeto.");
 }
+
+/*
+*
+audio
+*
+*/
+
+function audio(e){
+	
+	$("#b_playAudio").bind("tap", function(){
+		e.preventDefault();
+   		e.stopImmediatePropagation();
+		 $("#resultadoAudio").html("Pidiendo audio");
+ 			playAudio("http://leocondori.com.ar/app/audio/linea.mp3");
+	});
+	
+	$("#b_stopAudio").bind("tap", function(){
+		e.preventDefault();
+   		e.stopImmediatePropagation();
+ 			stopAudio();
+	});
+}
+	var my_media = null;
+    var mediaTimer = null;
+	
+	function playAudio(src) {
+         if (my_media == null) {
+              my_media = new Media(src, onSuccess, onError);
+         } 
+		 my_media.play();
+
+         if (mediaTimer == null) {
+              mediaTimer = setInterval(function() {
+                 my_media.getCurrentPosition(
+                        // success callback
+                        function(position) {
+                            if (position > -1) {
+								$("#resultadoAudio").html("Pos = " + position);
+                            }
+                        },
+                        // error callback
+                        function(e) {
+							$("#resultadoAudio").html("Error = " + e);
+                            setAudioPosition("Error: " + e);
+                        }
+                 );
+             }, 1000);
+         }
+    }
+
+	function stopAudio() {
+        if (my_media) {
+             my_media.stop();
+        }
+        clearInterval(mediaTimer);
+        mediaTimer = null;
+    }
+	
+	function onSuccess() {
+           $("#resultadoAudio").html("playAudio():Audio Success");
+    }
+
+     function onError(error) {
+         $("#resultadoAudio").html('code: '    + error.code  + '<br>' +
+                  'message: ' + error.message + '\n');
+      }
