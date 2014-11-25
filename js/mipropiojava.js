e9a1bc4b2df8901a090e3588b97a24b3091ecb32
yosoyelpipi <lcondori@gmail.com>
2014-11-24 22:55:20 -0300

    2 cambios red status
    red

--- js/mipropiojava.js
+++ js/mipropiojava.js
@@ -8,9 +8,39 @@ function onDeviceReady(){
 	$("#resultado").html("Phonegap está en funcionamiento.");
 
 	//navigator.accelerometer.getCurrentAcceleration(onInfo, onError);
-
+    checkConnection();
 	var watch = navigator.accelerometer.watchAcceleration (onInfo, onError, {frecuency: 2000});
 }
+
+/*
+*
+red
+*
+*/
+
+
+        function checkConnection() {
+            var networkState = navigator.connection.type;
+
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.CELL]     = 'Cell generic connection';
+            states[Connection.NONE]     = 'No network connection';
+
+            alert('Connection type: ' + states[networkState]);
+        }
+
+/*
+*
+acelerometro
+*
+*/
+
 
 function onInfo(aceleracion){
 $("#resultado").html( 'Acceleration X: ' + aceleracion.x + '<br>' +
